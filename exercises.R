4e8c5cea04dff7996450e80a78fa48cef9b5895b
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-06-16 13:12:10 +0200

    Add 5.5.2 exercises

--- exercises.R
+++ exercises.R
@@ -66,6 +66,25 @@ print('Exercises 5.4.1 (select)')
     # default? ANS: I am, they ignore case by default.
     select(flights, contains("TIME", ignore.case=FALSE))
 
+print('Exercises 5.5.2 (mutate)')
+    # Currently dep_time and sched_dep_time are convenient to look at, but hard
+    # to compute with because they’re not really continuous numbers. Convert
+    # them to a more convenient representation of number of minutes since
+    # midnight.
+
+    # Compare air_time with arr_time - dep_time. What do you expect to see?
+    # What do you see? What do you need to do to fix it?
+
+    # Compare dep_time, sched_dep_time, and dep_delay. How would you expect
+    # those three numbers to be related?
+
+    # Find the 10 most delayed flights using a ranking function. How do you
+    # want to handle ties? Carefully read the documentation for min_rank().
+
+    # What does 1:3 + 1:10 return? Why?
+
+    # What trigonometric functions does R provide?
+
 print('Exercises 5.6.7 (grouped summaries)')
 
 print('Exercises 5.7.1 (grouped mutates and filters)')
