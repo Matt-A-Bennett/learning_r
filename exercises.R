7d485806670ee4344af56db8ce246c38b06d6e3a
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-06-18 12:17:43 +0200

    Add figures for exercises 5.6.7 (grouped summaries)

--- exercises.R
+++ exercises.R
@@ -8,6 +8,7 @@ source("../functions.R")
 
 # print the tibble
 nycflights13::flights
+not_cancelled <- flights %>% filter(!is.na(dep_delay), !is.na(arr_delay))
 
 print('Exercises 5.2.4 (filter)')
     # Had an arrival delay of two or more hours
@@ -94,7 +95,60 @@ print('Exercises 5.5.2 (mutate)')
     # What trigonometric functions does R provide? ANS: defaults to degrees,
     # rather that radians... gives the sin, cos, tan + their inverses, atan2,
     # and versions for taking some proportion of pi (i.e. do things in radians)
+
+not_cancelled %>%
+    group_by(year, month, day) %>%
+    summarize(mean = mean(dep_delay), median = median(dep_delay)) %>%
+    arrange(desc(median))
+
+delays <- not_cancelled %>%
+    group_by(tailnum) %>%
+    summarize(
+              delay = mean(arr_delay, na.rm = TRUE),
+              n = n()
+    )
+
+delays %>% 
+    filter(n > 25) %>%
+    ggplot(mapping = aes(x = delay, y = n)) +
+    geom_point(alpha = 1/10)
+
 print('Exercises 5.6.7 (grouped summaries)')
+    # Brainstorm at least five different ways to assess the typical delay
+    # characteristics of a group of flights. Consider the following sce‐
+
+    # • A flight is 15 minutes early 50% of the time, and 15 minutes late 50%
+    # of the time.
+
+    # • A flight is always 10 minutes late.
+
+    # • A flight is 30 minutes early 50% of the time, and 30 minutes late 50%
+    # of the time.
+
+    # • 99% of the time a flight is on time. 1% of the time it’s 2 hours late.
+
+    # Which is more important: arrival delay or departure delay?
+
+    # 2. Come up with another approach that will give you the same output as
+    # not_cancelled %>% count(dest) and not_cancel led %>% count(tailnum, wt =
+    # distance) (without using count() ).
+
+    # 3. Our definition of cancelled flights ( is.na(dep_delay) |
+    # is.na(arr_delay)) is slightly suboptimal. Why? Which is the most
+    # important column?
+
+    # 4. Look at the number of cancelled flights per day. Is there a pat‐ tern?
+    # Is the proportion of cancelled flights related to the average delay?
+
+    # 5. Which carrier has the worst delays? Challenge: can you disen‐ tangle
+    # the effects of bad airports versus bad carriers? Why/why not? (Hint:
+    # think about flights %>% group_by(carrier, dest) %>% summarize(n()) .)
+
+    # 6. For each plane, count the number of flights before the first delay of
+    # greater than 1 hour.
+
+    # 7. What does the sort argument to count() do? When might you use it?
 
 print('Exercises 5.7.1 (grouped mutates and filters)')
+
 
