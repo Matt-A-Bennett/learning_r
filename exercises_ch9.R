a12bdf023fa67ff437a0d75fce07493d5f17bdcc
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-07-09 11:58:09 +0200

    Add exercises: spreading and gathering

--- exercises_ch9.R
+++ exercises_ch9.R
@@ -44,3 +44,49 @@ table2 %>%
     ggplot(aes(year, cases, group = country)) +
     geom_line() +
     geom_point(aes(color = country))
+
+### spreading and gathering ###################################################
+# 1. Why are gather() and spread() not perfectly symmetrical? Carefully
+# consider the following example:
+
+stocks <- tibble(
+                 year = c(2015, 2015, 2016, 2016),
+                 half = c( 1, 2, 1, 2),
+                 return = c(1.88, 0.59, 0.92, 0.17)
+)
+
+stocks %>%
+    spread(year, return) %>%
+    gather("year", "return", `2015`:`2016`)
+
+# (Hint: look at the variable types and think about column names.) Both
+# spread() and gather() have a convert argument. What does it do?
+
+# 2. Why does this code fail?
+
+table4a %>%
+    gather(1999, 2000, key = "year", value = "cases")
+#> Error in eval(expr, envir, enclos):
+#> Position must be between 0 and n
+
+# 3. Why does spreading this tibble fail? How could you add a new column to fix
+# the problem?
+
+people <- tribble(
+                  ~name,            ~key,    ~value,
+                  #-----------------|--------|------
+                  "Phillip Woods", "age", 45,
+                  "Phillip Woods", "height", 186,
+                  "Phillip Woods", "age", 50,
+                  "Jessica Cordero", "age", 37,
+                  "Jessica Cordero", "height", 156
+)
+
+# 4. Tidy this simple tibble. Do you need to spread or gather it? What are the
+# variables?
+
+preg <- tribble(
+                ~pregnant, ~male, ~female,
+                "yes", NA, 10,
+                "no", 20, 12
+)
