c3e287ee77f9c06c174c22e38011286e04d5a01f
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-07-31 12:45:59 +0200

    Decrease print width from 160 to 159

--- scratch.R
+++ scratch.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(Lahman)
 library(ggdark)
 source("../functions.R")
-options(width = 160)
+options(width = 159)
 theme_set(dark_theme_gray(base_size = 28)
           + theme(plot.background = element_rect(fill = "grey10"),
                   panel.grid.major = element_line(color = "grey30", size = 0.2),
