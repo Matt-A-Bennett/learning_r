bafa827254b0b84263d0493613d8af1be4fd0b51
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-08-06 12:20:04 +0200

    Add blank line before themes

--- scratch.R
+++ scratch.R
@@ -4,6 +4,7 @@ library(Lahman)
 library(ggdark)
 source("../functions.R")
 options(width = 159)
+
 theme_set(dark_theme_gray(base_size = 28)
           + theme(plot.background = element_rect(fill = "grey10"),
                   panel.grid.major = element_line(color = "grey30", size = 0.2),
@@ -11,5 +12,5 @@ theme_set(dark_theme_gray(base_size = 28)
 
 cl()
 
-colnames(flights)
+colnames(airports)
 airports
