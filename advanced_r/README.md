9cca20ddae52fa32ee559d9294c774ff0f46cdff
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-08-22 09:30:36 +0200

    Mulitple books now

---
+++ advanced_r/README.md
@@ -0,0 +1,6 @@
+# advanced_r
+Going through the exercises in [Advanced R](https://adv-r.hadley.nz/index.html)
+
+### Exercises Done so far:
+#### Chapter 2: Names and values
+- [ ] binding basics
---
+++ advanced_r/scratch.R
@@ -0,0 +1,9 @@
+source("../functions.R")
+options(width = 158)
+
+theme_set(dark_theme_gray(base_size = 28)
+          + theme(plot.background = element_rect(fill = "grey10"),
+                  panel.grid.major = element_line(color = "grey30", size = 0.2),
+                  panel.grid.minor = element_line(color = "grey30", size = 0.2)))
+
+cl()
